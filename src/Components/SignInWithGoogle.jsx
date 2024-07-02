14b95f2304f4087852ffe8bae9d6b71cea4f7cd2
Jennifer <jenniferpeterson@pursuit.org>
2024-07-02 11:12:42 -0400

    fixed the sign in with google code

--- src/Components/SignInWithGoogle.jsx
+++ src/Components/SignInWithGoogle.jsx
@@ -1,45 +1,57 @@
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import { setDoc, doc } from 'firebase/firestore'
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
-import { auth, db } from '../helpers/firebase'
-import googleBadge from '../assets/google.png'
+import { auth } from "../helpers/firebase";
+import { register } from "../helpers/register";
+import { fetchUser } from "../helpers/fetchUser";
+
+import googleBadge from "../assets/google.png";
+
+async function handleGoogleSignIn() {
+  const provider = new GoogleAuthProvider();
+  try {
+    //sign into Firebase
+    const { user } = await signInWithPopup(auth, provider);
+    //retrieve JWT token from firebase
+    const token = await user.getIdToken();
+    localStorage.setItem("token", token);
+
+    // Check if user exists in your backend
+    const foundUser = await fetchUser(user, token);
+    if (!foundUser.uid) {
+      // User does not exist in backend, create the user
+      const { photoURL, uid } = user;
+      await register(user, photoURL, uid);
+    }
+
+    // return key/value to use for the navigate in the googleLogin function below
+    return { navigateTo: "/profile" };
+  } catch (error) {
+    localStorage.removeItem("token");
+    throw error;
+  }
+}
 
 function SignInWithGoogle() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  function googleLogin() {
+  const googleLogin = async () => {
     try {
-      const provider = new GoogleAuthProvider()
-
-      signInWithPopup(auth, provider).then(async ({ user }) => {
-        const { email, displayName, photoURL } = user
-        if (user) {
-          await setDoc(doc(db, 'Users', user.uid), {
-            email,
-            firstName: displayName,
-            photo: photoURL,
-            lastName: '',
-          })
-
-          toast.success('User logged in Successfully', {
-            position: 'top-center',
-          })
-
-          navigate('/profile')
-        }
-      })
+      const result = await handleGoogleSignIn();
+      navigate(result.navigateTo);
     } catch (error) {
       toast.error(error.message, {
-        position: 'bottom-center',
-      })
+        position: "bottom-center",
+      });
     }
-  }
+  };
+
   return (
-    <div style={{ cursor: 'pointer' }} onClick={googleLogin}>
-      <img src={googleBadge} width={'20%'} />
+    <div style={{ cursor: "pointer" }} onClick={googleLogin}>
+      <img src={googleBadge} width={"20%"} />
     </div>
-  )
+  );
 }
-export default SignInWithGoogle
+
+export default SignInWithGoogle;
