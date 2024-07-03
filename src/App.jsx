c319f415b99311bd00bef655ff22f7945642df1f
Jennifer <jenniferpeterson@pursuit.org>
2024-07-03 10:51:59 -0400

    created the routes for the cookbook and favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-import { useEffect, useState } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
+import { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-import { auth } from './helpers/firebase'
+import { auth } from "./helpers/firebase";
 
-import Login from './Components/Login'
-import SignUp from './Components/Register'
-import Profile from './Components/Profile'
-import Test from './Components/Test'
+import Login from "./Components/Login";
+import SignUp from "./Components/Register";
+import Profile from "./Components/Profile";
+import Test from "./Components/Test";
 
-import 'react-toastify/dist/ReactToastify.css'
-import './App.css'
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+import FamilyForm from "./Components/Family/FamilyForm";
+import JoinFamilyForm from "./Components/Family/JoinFamilyForm";
+import FamilyCookbook from "./Components/Pages/FamilyCookbook";
+import Favortites from "./Components/Pages/Favortites";
 
 function App() {
-  const [user, setUser] = useState()
+  const [user, setUser] = useState();
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      setUser(user)
-    })
-  })
+      setUser(user);
+    });
+  });
   return (
     <div>
       <Routes
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
           marginTop: 100,
         }}
       >
@@ -38,10 +43,15 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
+        {/* Need to fix the paths below */}
+        <Route path="/familyform" element={<FamilyForm />} />
+        <Route path="/join_family" element={<JoinFamilyForm />} />
+        <Route path="/family_cookbook" element={<FamilyCookbook />} />
+        <Route path="/favorites" element={<Favortites />} />
       </Routes>
       <ToastContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
