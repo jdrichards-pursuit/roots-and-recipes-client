dbb764e8b4e8f60626ade2e13d91c5aad39bb9b9
Jennifer <jenniferpeterson@pursuit.org>
2024-07-03 11:29:33 -0400

    created the navbar component

---
+++ src/Components/Pages/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PlusIcon from "./PlusIcon";
+
+const NavBar = () => {
+  return (
+    <div>
+      <PlusIcon />
+      <div>NavBar</div>
+    </div>
+  );
+};
+
+export default NavBar;
