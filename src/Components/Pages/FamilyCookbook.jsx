0aabbd1f6383854e1a6053f2ded1fdeb7e8fc985
Jennifer <jenniferpeterson@pursuit.org>
2024-07-03 10:51:20 -0400

    created the family cookbook and the favorites components

---
+++ src/Components/Pages/FamilyCookbook.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FamilyCookbook = () => {
+  return <div>FamilyCookbook</div>;
+};
+
+export default FamilyCookbook;
---
+++ src/Components/Pages/Favortites.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Favortites = () => {
+  return <div>Favortites</div>;
+};
+
+export default Favortites;
