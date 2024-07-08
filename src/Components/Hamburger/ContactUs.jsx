798402b598c8add6494cac8a8cbc04906937cd3c
Jennifer <jenniferpeterson@pursuit.org>
2024-07-08 10:13:26 -0400

    installed tailwind and added some background colors to different components

--- src/Components/Hamburger/ContactUs.jsx
+++ src/Components/Hamburger/ContactUs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return <div className="bg-orange-200">ContactUs</div>;
 };
 
 export default ContactUs;
--- src/Components/Recipe/RecipeForm.jsx
+++ src/Components/Recipe/RecipeForm.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { getUserData } from "../../helpers/getUserData";
 
 const RecipeForm = () => {
-  return <div>RecipeForm</div>;
+  const [userDetails, setUserDetails] = useState(null);
+
+  useEffect(() => {
+    async function getUser() {
+      // this is a helper function that will check the state of the current user in firebase and fetch the user using the JWT token from localstorage and the uid
+      const user = await getUserData();
+      // console.log("useEffect Profile:", user);
+      if (user) setUserDetails(user);
+    }
+
+    getUser();
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      {/* {console.log(userDetails)} */}
+      {userDetails ? (
+        <>
+          <img
+            src={userDetails.photo || placeholderImage}
+            alt={userDetails.first_name}
+            style={{
+              marginTop: 100,
+              marginBottom: 20,
+              borderRadius: "50%",
+              width: 150,
+              height: 150,
+            }}
+          />
+
+          <h1>{userDetails.first_name}'s Profile Page</h1>
+
+          <p>Email: {userDetails.email}</p>
+          <p>First Name: {userDetails.first_name}</p>
+          <p>
+            Last Name:{" "}
+            {userDetails.last_name ? userDetails.last_name : "Unknown"}
+          </p>
+          <p>Nickname: {userDetails.nickname}</p>
+
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <h2>Loading...</h2>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default RecipeForm;
