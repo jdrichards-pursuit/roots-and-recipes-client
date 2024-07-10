b6bf7a2e77d82d8a0a61dced8b74692a7b3c4862
Jennifer <jenniferpeterson@pursuit.org>
2024-07-10 16:37:29 -0400

    setting up to make the fetch call in the cookbook component

--- src/Components/Pages/MyCookbook.jsx
+++ src/Components/Pages/MyCookbook.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { getAuth } from "firebase/auth";
 
 export const MyCookbook = () => {
-  return <div>MyCookbook</div>;
+  // need to use useAuth for the user
+  // create a state called "usersUID" and then set the initial value to user.currentUser.uid
+  // make a fetch call to "/user/:uid" and give the state as the uid value (template literals)
+  // store this data into a user state
+
+  // we need this to have access to the users information that is protected by firebase
+  const user = getAuth();
+
+  return (
+    <div className="text-center">
+      <div>MyCookbook</div>
+    </div>
+  );
 };
