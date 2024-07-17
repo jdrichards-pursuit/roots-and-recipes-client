366781260a36867f4d96b5edd004d4114a66a892
Iris Beckham <irisdancer3@gmail.com>
2024-07-17 15:06:35 -0400

    open ai api call to get ingredients

--- src/Components/Recipe/ImagetoText.jsx
+++ src/Components/Recipe/ImagetoText.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Tesseract from 'tesseract.js';
+import { Link } from "react-router-dom";
+import OpenAI from "openai";
+const URL = import.meta.env.VITE_OPENAI_API_KEY;
+
+
+const openai = new OpenAI({ apiKey: URL, dangerouslyAllowBrowser: true });
 
 //lines 12-28: https://github.com/RamanSharma100/react-image-to-text/blob/main/src/App.js
 
@@ -12,7 +18,7 @@ const ImagetoText = ({ image }) => {
         setIsLoading(true);
         Tesseract.recognize(image, 'eng', {
             logger: (m) => {
-                console.log(m);
+                // console.log(m);
                 if (m.status === 'recognizing text') {
                     setProgress(parseInt(m.progress * 100));
                 }
@@ -25,12 +31,22 @@ const ImagetoText = ({ image }) => {
                 // console.log(result.data);
                 setOcr(result.data.text);
                 setIsLoading(false);
+                getIngredients(result.data.text)
             });
+    }
+
+    async function getIngredients() {
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "system", content: `Extract all the ingredients from this text and format the output in JSON with the name as a string and quantity as an integer: ${ocr}`, "type": "json_object" }],
+            model: "gpt-3.5-turbo",
+        });
+
+        console.log(completion.choices[0]);
     }
 
     return (
         <div>
-            <button onClick={extractText}>convert </button>
+            <button onClick={extractText}>Convert to Text</button>
             {isLoading && (
                 <>
                     <progress className="form-control" value={progress} max="100">
@@ -41,11 +57,13 @@ const ImagetoText = ({ image }) => {
             )}
             {!isLoading && ocr && (
                 <>
+                    {/* <Link to={`/recipe_form`}> */}
                     <textarea
                         rows="15"
                         value={ocr}
                         onChange={(e) => setOcr(e.target.value)}
                     ></textarea>
+                    {/* </Link> */}
                 </>
             )}
         </div>
