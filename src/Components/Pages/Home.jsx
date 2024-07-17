1909ba57ba13fe8c50ceb3f00ca078e0341378bb
Jennifer <jenniferpeterson@pursuit.org>
2024-07-17 17:01:30 -0400

    storing the selected recipe in a state but need to access in the showPage

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,17 +1,32 @@
 import { useState, useEffect } from "react";
 import { getAuth } from "firebase/auth";
-
+import useHandleSearchChange from "../../helpers/useHandleSearchChange";
+import { Link } from "react-router-dom";
 const URL = import.meta.env.VITE_BASE_URL;
+import { useNavigate } from "react-router-dom";
+import RecipeShow from "../Recipe/RecipeShow";
 
 const Home = ({ setBurgerToggle }) => {
+  const navigate = useNavigate();
   const [allPublicRecipes, setAllPublicRecipes] = useState([]);
   const [allLunchRecipes, setAllLunchRecipes] = useState([]);
   const [allDinnerRecipes, setAllDinnerRecipes] = useState([]);
   const [homeDefault, setHomeDefault] = useState(true);
   const [searchedRecipes, setSearchedRecipes] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const { searchInput, handleSearchChange } = useHandleSearchChange(
+    allPublicRecipes,
+    setSearchedRecipes,
+    setHomeDefault
+  );
 
   const user = getAuth();
+
+  const handleClick = (recipe) => {
+    setSelectedRecipe(recipe);
+    // navigate(`/recipe_show/${recipe.id}`);
+    console.log(`"i just clicked:"`, recipe);
+  };
 
   useEffect(() => {
     fetch(`${URL}/api/recipes`)
@@ -35,24 +50,13 @@ const Home = ({ setBurgerToggle }) => {
     setBurgerToggle(false);
   }, []);
 
-  const handleSearchChange = (e) => {
-    const search = e.target.value;
-    setSearchInput(search);
-    const result = search.length
-      ? allPublicRecipes.filter((recipe) =>
-          recipe.name.toLowerCase().includes(search.toLowerCase())
-        )
-      : [];
-    setSearchedRecipes(result);
-    setHomeDefault(search.length === 0);
-  };
-
   return (
     <div className="p-4">
       <div className="mb-4">
         <input
           type="text"
           placeholder="Search"
+          value={searchInput}
           onChange={handleSearchChange}
           className="w-full p-2 border rounded"
         />
@@ -63,24 +67,26 @@ const Home = ({ setBurgerToggle }) => {
           <h1 className="text-xl font-bold mb-4">Top Lunch Recipes</h1>
           <div className="flex overflow-x-auto space-x-4">
             {allLunchRecipes.map((r, index) => (
-              <div key={index} className="flex-shrink-0">
-                <p className="text-center mb-2">{r.name}</p>
-                <div className="w-48 h-48 relative">
-                  <img
-                    key={index}
-                    src={r.photo}
-                    alt={`Recipe ${r.photo}`}
-                    className="object-cover w-full h-full rounded"
-                  />
+              <Link key={index} to={<RecipeShow />}>
+                <div className="flex-shrink-0">
+                  <p className="text-center mb-2">{r.name}</p>
+                  <div className="w-48 h-48 relative">
+                    <img
+                      key={index}
+                      src={r.photo}
+                      alt={`Recipe ${r.photo}`}
+                      className="object-cover w-full h-full rounded"
+                    />
+                  </div>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
 
           <h1 className="text-xl font-bold mt-8 mb-4">Top Dinner Recipes</h1>
           <div className="flex overflow-x-auto space-x-4">
             {allDinnerRecipes.map((r, index) => (
-              <div key={index} className="flex-shrink-0">
+              <div onClick={() => handleClick(r)} className="flex-shrink-0">
                 <p className="text-center mb-2">{r.name}</p>
                 <div className="w-48 h-48 relative">
                   <img
@@ -91,12 +97,15 @@ const Home = ({ setBurgerToggle }) => {
                   />
                 </div>
               </div>
+              // </Link>
             ))}
           </div>
         </>
       )}
 
-      {!homeDefault && (
+      {!homeDefault && selectedRecipe && <RecipeShow recipe={selectedRecipe} />}
+
+      {!homeDefault && !selectedRecipe && (
         <>
           {searchedRecipes.length === 0 ? (
             <p className="text-center bg-gray-200 p-4">
