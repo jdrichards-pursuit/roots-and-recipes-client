403a4ab04c73a7bec94d234268b9530f3d572c8b
Jennifer <jenniferpeterson@pursuit.org>
2024-07-19 19:23:43 -0400

    made it where adding a nickname is OPTIONAL for the user

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -159,7 +159,6 @@ function Register() {
               placeholder="Enter username"
               value={newUser.nickname}
               onChange={handleChange}
-              required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
