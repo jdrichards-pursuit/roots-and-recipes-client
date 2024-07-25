050e093f4a7acfd928d494c37885c15df032c019
Iris Beckham <irisdancer3@gmail.com>
2024-07-25 11:23:17 -0600

    upload recipe photo to cloudinary and set the image as the new link

--- src/Components/Recipe/TakePhoto.jsx
+++ src/Components/Recipe/TakePhoto.jsx
@@ -9,11 +9,33 @@ const TakePhoto = () => {
     const [image, setImage] = useState('');
     const [imageURL, setImageURL] = useState('')
     const [showCamera, setShowCamera] = useState(false);
+    const [convertToText, setConvertToText] = useState(false);
+
+    const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET_KEY;
+    const cloudName = import.meta.env.VITE_CLOUD_NAME;
+
+    async function uploadImagetoCloudinary() {
+        const recipeImage = new FormData();
+        recipeImage.append("file", image);
+        recipeImage.append("cloud_name", cloudName);
+        recipeImage.append("upload_preset", uploadPreset);
+        const response = await fetch(
+            `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+            { method: "POST", body: recipeImage }
+        );
+
+        const data = await response.json();
+        console.log(data.url);
+        const imgUrl = data.url.toString();
+        setImage(imgUrl)
+        setConvertToText(true)
+    }
 
     function handleTakePhotoAnimationDone(dataUri) {
         setImageURL('')
         setImage(dataUri)
         setShowCamera(false);
+        console.log(dataUri)
     }
 
     function retake() {
@@ -24,6 +46,7 @@ const TakePhoto = () => {
         setImage(e.target.files[0])
         const objectUrl = URL.createObjectURL(e.target.files[0])
         setImageURL(objectUrl)
+        console.log(objectUrl.slice(5))
     }
 
     function handleCameraChange() {
@@ -51,7 +74,8 @@ const TakePhoto = () => {
                         <img src={imageURL}></img>
                         : <img src={image}></img>}
                     <button onClick={retake}>retake</button>
-                    <ImagetoText image={image} />
+                    <button onClick={uploadImagetoCloudinary()}>Use this Image</button>
+                    {convertToText && <ImagetoText image={image} />}
                 </div>}
         </div>
     )
