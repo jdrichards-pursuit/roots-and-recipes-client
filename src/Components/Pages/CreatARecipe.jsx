52e97a71a44d0c20f050b414a001624bbfd24f14
Iris Beckham <irisdancer3@gmail.com>
2024-07-25 11:19:07 -0600

    removed i icon for more information

--- src/Components/Pages/CreatARecipe.jsx
+++ src/Components/Pages/CreatARecipe.jsx
@@ -1,5 +1,4 @@
-import { Info } from "lucide-react";
-import { Camera } from "lucide-react";
+
 import { ChevronsRight } from "lucide-react";
 import { Link } from "react-router-dom";
 import TakePhoto from "../Recipe/TakePhoto"
@@ -11,9 +10,6 @@ const CreatARecipe = () => {
       <div className="border-solid border-2 border-black p-2 items-center text-center text-[#713A3A] bg-[#FFEFB9] rounded-2xl">
         <div className="flex justify-between mb-2">
           <h2 className="flex-1">Add a Recipe</h2>
-          <div className="ml-4">
-            <Info />
-          </div>
         </div>
         <TakePhoto />
       </div>
