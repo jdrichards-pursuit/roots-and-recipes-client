56e4f471ae2d240108f1a1059b333dd725ef88fa
Iris Beckham <irisdancer3@gmail.com>
2024-07-29 11:17:38 -0600

    removed the numbers before the steps

--- src/helpers/openAI.js
+++ src/helpers/openAI.js
@@ -20,10 +20,10 @@ export async function callOpenAI(image) {
                     role: "system",
                     content: [{
                         type: "text",
-                        text: `You are an AI that extracts the ingredients and steps from an image with text. The image has been uploaded to Cloudinary and can be accessed at the given url. Only use the instructions provided and return a JSON object. 
+                        text: `You are an AI that extracts the ingredients and steps from an image with text. The image has been uploaded to Cloudinary and can be accessed at the given url. Only use the instructions provided and return a JSON object. Remove any markers that signify that the steps are in an ordered list. 
                         Fill the JSON template below:
                         {"ingredients": ["2 eggs", "3 3/4 cups milk", "1/2 tablespoons honey"],
-                        "steps": ["1. Put in the flour.", "2. Mix the dry ingredients", "3. Crack the eggs in the bowl"],
+                        "steps": ["Put in the flour.", "Mix the dry ingredients", "Crack the eggs in the bowl"],
                         "prompt": "Whatever."}`}]
                 },
                 {
