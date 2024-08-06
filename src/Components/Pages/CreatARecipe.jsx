fc078b6df3b5d6f0dd8c10b0025d9837a5fb2a2e
Haiyah Perez <haiyahperez@pursuit.org>
2024-08-06 17:23:08 -0400

    add styling

--- src/Components/Pages/CreatARecipe.jsx
+++ src/Components/Pages/CreatARecipe.jsx
@@ -5,14 +5,14 @@ import TakePhoto from "../Recipe/TakePhoto";
 const CreatARecipe = () => {
   return (
     <div className="flex flex-col items-center min-h-screen py-8">
-      <div className="bg-[#FFDAB9] p-4 text-center text-[#713A3A] rounded-lg shadow-lg w-full max-w-md">
+      <div className="bg-[#FFDAB9] bg-opacity-90 backdrop-blur-md p-6 rounded-xl shadow-lg w-full max-w-lg">
         <TakePhoto />
       </div>
-      <div className="mt-8">
+      <div className="mt-10">
         <Link to={`/recipe_form`}>
-          <div className="bg-[#C7DEF1] flex items-center justify-between mx-auto w-80 border-2 border-[#713A3A] rounded-xl p-2 shadow-lg transition-transform transform hover:scale-105">
-            <p className="text-[#713A3A] font-medium">Enter Manually</p>
-            <ChevronsRight className="text-[#713A3A]" />
+          <div className="bg-blue-600 flex items-center justify-between mx-auto w-80 border-2 rounded-xl p-3 shadow-lg transition-all duration-200  bg-blue-600font-semibold py-3 rounded-lg hover:bg-blue-700">
+            <p className="text-white font-semibold">Enter Manually</p>
+            <ChevronsRight className="text-white" />
           </div>
         </Link>
       </div>
